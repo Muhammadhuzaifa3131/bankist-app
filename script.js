b827f97b03920d591a045a733b3f524ee552b65a
Muhammad Bin Atif <huzaifa7893131@gmail.com>
2026-09-08 08:13:05 +0500

    Refactor input parsing to use unary plus operator
    Updated input conversion for pin and amounts to use unary plus operator for type coercion.

--- script.js
+++ script.js
@@ -138,7 +138,7 @@ btnLogin.addEventListener('click', function(e){
   currentAccount = accounts.find(acc => acc.userName === inputLoginUsername.value);
   // console.log(currentAccount);
 
-  if(currentAccount?.pin === Number(inputLoginPin.value)){
+  if(currentAccount?.pin === +(inputLoginPin.value)){
     // Display UI and Message
     labelWelcome.textContent = `Welcome Back, ${currentAccount.owner.split(' ')[0]}`
     containerApp.style.opacity = 100;
@@ -155,7 +155,7 @@ btnLogin.addEventListener('click', function(e){
 
 btnTransfer.addEventListener('click', function(e){
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +(inputTransferAmount.value);
   const receiverAcc = accounts.find(acc => acc.userName === inputTransferTo.value);
 
   inputTransferAmount.value = inputTransferTo.value  = ''
@@ -174,7 +174,7 @@ btnTransfer.addEventListener('click', function(e){
 btnLoan.addEventListener('click', function(e){
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if(amount > 0 && currentAccount.movements.some(mov => mov >= amount *0.1)){
     // Add the movemetnt
@@ -190,7 +190,7 @@ btnLoan.addEventListener('click', function(e){
 btnClose.addEventListener('click', function(e){
   e.preventDefault('');
   
-  if(currentAccount.userName === inputCloseUsername.value && currentAccount.pin === Number(inputClosePin.value)){
+  if(currentAccount.userName === inputCloseUsername.value && currentAccount.pin === +(inputClosePin.value)){
     const index = accounts.findIndex(acc => acc.userName === currentAccount.userName)
     // console.log(index);
     
