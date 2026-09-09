ebf9e6e74d502414c67d91b86cdf82e55f4217d5
Muhammad Bin Atif <huzaifa7893131@gmail.com>
2026-09-09 16:42:23 +0500

    Set current date and time on label
    Add date and time display to the label

--- script.js
+++ script.js
@@ -131,6 +131,16 @@ const updateUI = function(acc){
 // Event Handler
 let currentAccount; 
 
+// Set Date on label
+const now = new Date();
+const day = `${now.getDate()}`.padStart(2,0);
+const month = `${now.getMonth()+1}`.padStart(2,0);;
+const year = now.getFullYear();
+const hour = now.getHours();
+const min = now.getMinutes();
+labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`
+
+
 btnLogin.addEventListener('click', function(e){
   // Prevent Form submitting
   e.preventDefault();
