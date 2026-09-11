5969a040ae1aa802e7fea6b7ce7e53a1e86e804d
Muhammad Bin Atif <huzaifa7893131@gmail.com>
2026-09-12 00:07:59 +0500

    Add delay for loan movement processing
    Delay the addition of loan movement and update UI.

--- script.js
+++ script.js
@@ -276,14 +276,16 @@ btnLoan.addEventListener('click', function(e){
   const amount = Math.floor(inputLoanAmount.value);
 
   if(amount > 0 && currentAccount.movements.some(mov => mov >= amount *0.1)){
-    // Add the movemetnt
-    currentAccount.movements.push(amount);
-
-    // Add Loan Date
-    currentAccount.movementsDates.push(new Date().toISOString())
-
-    // Update Ui
-    updateUI(currentAccount)
+    setTimeout(function(){
+      // Add movement
+      currentAccount.movements.push(amount);
+  
+      // Add Loan Date
+      currentAccount.movementsDates.push(new Date().toISOString())
+  
+      // Update UI
+      updateUI(currentAccount);
+    }, 2000);
   }
   inputLoanAmount.value = ''
 })
