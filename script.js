d38d52bdc26aa32a67bb9e9e59833a86aa49782c
Muhammad Bin Atif <huzaifa7893131@gmail.com>
2026-09-12 08:44:02 +0500

    Update account owners' names in script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Muhammad Huzaifa',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
@@ -22,7 +22,7 @@ currency: 'USD',
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: 'Umer Khattab',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
@@ -41,7 +41,7 @@ const account2 = {
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
+  owner: 'Muhammad Bin Atif',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
@@ -60,7 +60,7 @@ const account3 = {
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
+  owner: 'Muhammad Ali',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
